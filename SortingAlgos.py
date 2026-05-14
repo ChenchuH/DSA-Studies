02ec960100442492d6850dd27e8093cbf435c593
ChenchuH <ynchenchuh@icloud.com>
2026-05-13 23:01:38 -0700

    completed quadratic sorts

--- SortingAlgos.py
+++ SortingAlgos.py
@@ -24,9 +24,25 @@ def selection_sort(arr):
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
     return arr
 
+#Insertion sort, moves larger elements to the right to make space, and the current value ends up moving left into its correct position.
+def insertion_sort(arr):
+    arr = arr.copy()
+    for i in range(1, len(arr)): #starts range from 1 - len of array ex. arr[10] range is now equal to 1 to 9 instead of 0-9
+        val = arr[i] #sets value equal to value of index 1 instead of zero as usual
+        j = i - 1 #j is now bieng shifted to be the index always to the left of i, in this case 0
+        while j >=0 and arr[j]>val: #while j is greater than or is the index 0 and the value of index j is greater than the value of index i
+            arr[j+1]=arr[j] #make the value of index j move to the right
+            j-=1 #decreases j index by 1
+        arr[j+1] = val #assigns value of arr[i] to the index left of arr[j]
+    return arr
+
+
+
 arr = array_gen(10)
-arr_sort = bubble_sort(arr)
+bubble_sort = bubble_sort(arr)
 selection_sort = selection_sort(arr)
+insertion_sort = insertion_sort(arr)
 print(f"Original array",arr)
-print(f"Bubble sort: ",arr_sort)
-print(f"selection sort: ",selection_sort)
+print(f"Bubble sort: ",bubble_sort)
+print(f"Selection sort: ",selection_sort)
+print(f"Insertion sort: ",insertion_sort)
